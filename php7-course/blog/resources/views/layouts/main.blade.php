9653e17d3ce51024a1242ee990a4f9d24169cb07
emiliesedziol <sedziole@gmail.com>
2018-06-11 10:51:57 -0400

    css laravel

--- php7-course/blog/resources/views/layouts/main.blade.php
+++ php7-course/blog/resources/views/layouts/main.blade.php
@@ -1,22 +1,26 @@
 <html>
     <head>
         <title>App Name - @yield('title')</title>
+      <!--  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">   -->
+        <link href="/css/app.css" rel="stylesheet">
     </head>
     <body>
       <header>
-        <nav>
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Search</a>
-          <a href="#">Contact</a>
-        </nav>
-      </header>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
-
         <div class="container">
-            @yield('content')
+          <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Search</a>
+            <a href="#">Contact</a>
+          </nav>
         </div>
+      </header>
+
+
+
+          <div class="container">
+              @yield('content')
+          </div>
+
     </body>
 </html>
