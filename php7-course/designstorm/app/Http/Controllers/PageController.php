706473ae4c719e7a4d2a9ca2f750d615817e956c
emiliesedziol <sedziole@gmail.com>
2018-06-14 20:39:20 -0400

    setting up an API connection

--- php7-course/designstorm/app/Http/Controllers/PageController.php
+++ php7-course/designstorm/app/Http/Controllers/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use GuzzleHttp\Client;
 
 
 // public function __construct()
@@ -19,6 +20,26 @@ class PageController extends Controller
     }
 
     public function results() {
+      // 'https://api.behance.net/v2/projects?q=dog&client_id=oDlfZpUkt2LxvuOuymMieOpTCXUUMQvR';
+      $client = new Client();
+
+      $res = $client->request('GET', 'https://api.behance.net/v2/projects?q=dog&client_id=oDlfZpUkt2LxvuOuymMieOpTCXUUMQvR');
+
+      $data = $res->getBody();
+      $data = json_decode($data);
+      $filteredData = [];
+
+      // return $data->projects;
+      foreach($data->projects as $project) {
+        $fields = $project->fields;
+
+        if(in_array("UI/UX", $fields) || in_array("Web Design", $fields)) {
+          array_push($filteredData, $project);
+        }
+      }
+
+      return count($filteredData);
+
       $user = Auth::user();
       return view('pages/results', compact('user'));
     }
